6b911e5f1b0619d96b2bb659c61f825c5b060e29
Olena Sukhanova <asukhanova87@gmail.com>
2020-06-21 13:42:43 +0300

    created Homepage

---
+++ app.js
@@ -0,0 +1,54 @@
+
+// Slider
+
+$(".slider_container").slick(  {
+   dots:true,
+   autoplay:true,
+   autoplaySpeed:2000,
+   arrows:false,
+   // adaptiveHeight: true,
+   pauseOnDotsHover:true,
+   zindex:10,
+});
+
+$('.slider_feedback_section').slick({
+   infinite: true,
+   slidesToShow: 3,
+   slidesToScroll: 3,
+   arrows:true,
+   autoplay:true,
+   autoplaySpeed:2000,
+   adaptiveHeight: true,
+   pauseOnDotsHover:true,
+   responsive: [
+      {
+        breakpoint:1024,
+        settings:{
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint:800,
+        settings:{
+          slidesToShow:2,
+          slidesToScroll:2,
+          dots:true,
+          arrows:false,
+        }
+      },
+      {
+        breakpoint:500,
+        settings:{
+          slidesToShow:1,
+          slidesToScroll:1,
+          dots:true,
+          arrows:false,
+          autoplaySpeed:5000,
+          centerMode:true,
+          centerPadding:'20px',
+        }
+      }
+    ]
+ });
+
