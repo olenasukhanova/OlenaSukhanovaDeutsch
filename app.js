f605346051cb3ea2518b197027069dcb4f242fca
Olena Sukhanova <asukhanova87@gmail.com>
2020-06-25 22:13:51 +0300

    changed active button

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("active1");
 
 
     let panel = this.nextElementSibling;
