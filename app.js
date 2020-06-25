76293628031e13f874d359119a23a60144a38efd
Olena Sukhanova <asukhanova87@gmail.com>
2020-06-25 22:08:04 +0300

    aded Service page

--- app.js
+++ app.js
@@ -53,6 +53,22 @@ $('.slider_feedback_section').slick({
  });
 
 
-//  let link = document.querySelector('link[rel=import]');
-//  let content = link.import.querySelector('#intro-dm');
-//  document.body.appendChild(content.cloneNode(true));
+//  products-buttons
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+
+
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
