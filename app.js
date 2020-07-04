c6d1a455073036b66ab17b8733e079ddcd887e6f
Olena Sukhanova <asukhanova87@gmail.com>
2020-07-04 19:03:33 +0300

    added UP-button, curtains, in contacts page - background-img library

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
-
 // Slider
-$('.slider_feedback_section').slick({
-   infinite: true,
-   slidesToShow: 3,
-   slidesToScroll: 3,
-   arrows:true,
-   autoplay:true,
-   autoplaySpeed:2000,
-   adaptiveHeight: true,
-   pauseOnDotsHover:true,
-   responsive: [
-      {
-        breakpoint:1024,
-        settings:{
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        }
-      },
-      {
-        breakpoint:800,
-        settings:{
-          slidesToShow:2,
-          slidesToScroll:2,
-          dots:true,
-          arrows:false,
-        }
-      },
-      {
-        breakpoint:500,
-        settings:{
-          slidesToShow:1,
-          slidesToScroll:1,
-          dots:true,
-          arrows:false,
-          autoplaySpeed:5000,
-          centerMode:true,
-          centerPadding:'20px',
-        }
-      }
-    ]
- });
+// $('.slider_feedback_section').slick({
+//    infinite: true,
+//    slidesToShow: 3,
+//    slidesToScroll: 3,
+//    arrows:true,
+//    autoplay:true,
+//    autoplaySpeed:2000,
+//    adaptiveHeight: true,
+//    pauseOnDotsHover:true,
+//    responsive: [
+//       {
+//         breakpoint:1024,
+//         settings:{
+//           slidesToShow: 3,
+//           slidesToScroll: 3,
+//         }
+//       },
+//       {
+//         breakpoint:800,
+//         settings:{
+//           slidesToShow:2,
+//           slidesToScroll:2,
+//           dots:true,
+//           arrows:false,
+//         }
+//       },
+//       {
+//         breakpoint:500,
+//         settings:{
+//           slidesToShow:1,
+//           slidesToScroll:1,
+//           dots:true,
+//           arrows:false,
+//           autoplaySpeed:5000,
+//           centerMode:true,
+//           centerPadding:'20px',
+//         }
+//       }
+//     ]
+//  });
 
 
-//  products-buttons
+// //  products-buttons
 let acc = document.getElementsByClassName("accordion");
 let i;
 
@@ -80,5 +79,48 @@ window.onclick = function(event) {
 }
 
 
+// slider 
+$(function(){
+                
+  $('#slider1').Thumbelina({
+      $bwdBut:$('#slider1 .left'),    
+      $fwdBut:$('#slider1 .right'),
+  });
+  
+})
 
 
+
+// btn
+
+// var controller = new ScrollMagic.Controller();
+
+// new ScrollMagic.Scene({
+//    triggerElement: "#multiDirect",
+//     duration: 200, 
+//     offset: 10, 
+// })
+// .setPin("#animate")
+// .addTo(controller);
+
+
+// squise-curtains
+$(function() {
+  $('[data-wanker]').wanker({delay: 200, duration: 2000});
+});
+
+
+// UP-button
+$(document).ready(function () {
+  $.goup({
+      trigger: 100,
+      bottomOffset: 150,
+      locationOffset: 100,
+      title: 'ВВЕРХ',
+      titleAsText: true,
+      zIndex:100,
+      containerRadius:50,
+      containerColor:"#c71806",
+      entryAnimation:"slide",
+  });
+});
